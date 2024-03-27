12f8742c8b8e7e66fc1690a653d7bf94211f20c1
anurag singh <anurag6569201@gmail.com>
2024-03-27 21:31:26 +0530

    added form for file uploading

--- core/views.py
+++ core/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import FileUploadForm
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        form = FileUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            contact_method = request.POST.get('contact_method')
+            # print(contact_method)
+            
+            return redirect('core:analysis')
+    else:
+        form = FileUploadForm()
     return render(request,"core/index.html")
+
+def analysis(request):
+    return render(request,"core/analysis.html")
