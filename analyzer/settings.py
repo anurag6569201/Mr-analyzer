0d91c5c9d30c21ea7b6290dd57041f3e1b287172
anurag singh <anurag6569201@gmail.com>
2024-03-31 02:41:09 +0530

    updated

--- analyzer/settings.py
+++ analyzer/settings.py
@@ -129,6 +129,7 @@ STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+STATICFILES_STORAGE='whitenoise.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
