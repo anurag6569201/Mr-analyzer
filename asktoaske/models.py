e86c6e0d8250f6297b40cb9619c88d49dc9578e0
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-07-24 20:26:23 +0530

    added env

--- asktoaske/models.py
+++ asktoaske/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import FileExtensionValidator
+
+def validate_file_size(value):
+    limit_mb = 2
+    if value.size > limit_mb * 1024 * 1024:
+        raise ValidationError(f"Max size of file is {limit_mb}MB")
 
 class PDFDocument(models.Model):
-    pdf_file = models.FileField(upload_to='pdfs/')
+    pdf_file = models.FileField(
+        upload_to='pdfs/',
+        validators=[FileExtensionValidator(allowed_extensions=['pdf']), validate_file_size]
+    )
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
-    
+        return self.pdf_file.name
