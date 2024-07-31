d12d6e8c5c5eaa85a9add6f414482191cbde9e1f
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-07-31 17:43:29 +0530

    triying for the default database

--- analyzer/settings.py
+++ analyzer/settings.py
@@ -89,18 +89,18 @@ WSGI_APPLICATION = 'analyzer.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mr-analyzer', 
-        'USER': 'anurag',
-        'PASSWORD': '6569as@AS', 
-        'HOST': 'azure-mysql-6569.mysql.database.azure.com',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'mr-analyzer', 
+    #     'USER': 'anurag',
+    #     'PASSWORD': '6569as@AS', 
+    #     'HOST': 'azure-mysql-6569.mysql.database.azure.com',
+    #     'PORT': '3306',
+    # },
 }
 
 
